2c97f3f8469c1f06f9eef2592d89faf19858107d
Chris Marshall <cmarshall10450@gmail.com>
2017-11-10 16:52:20 +0000

    Added tests for calculating the total number of animmals in a zoo.

--- tests/zoo/ZooTest.java
+++ tests/zoo/ZooTest.java
@@ -4,7 +4,7 @@ import animals.Elephant;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 public class ZooTest {
 
@@ -27,6 +27,31 @@ public class ZooTest {
     zoo.createEnclosureWithAnimals("Elephants", elephant);
 
     assertNotNull(zoo.getEnclosure("Elephants"));
+  }
+
+  @Test
+  public void testCanRemoveEnclosure() {
+    String name = "Elephants";
+    zoo.createEnclosure(name);
+    zoo.removeEnclosure(name);
+
+    assertNull(zoo.getEnclosure(name));
+  }
+
+  @Test
+  public void testCanGetEnclosure() {
+    String name = "Elephants";
+    zoo.createEnclosure(name);
+    assertNotNull(zoo.getEnclosure(name));
+  }
+
+  @Test
+  public void testCanGetTotalNumberOfAnimals() {
+    Elephant elephant = new Elephant("Shep", 1000);
+    zoo.createEnclosureWithAnimals("Elephants", elephant);
+
+    int actual = zoo.getTotalNumberOfAnimals();
+    assertEquals(1, actual);
   }
 
 }
